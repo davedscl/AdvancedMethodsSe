4d12e1887d4cec9434d239a347a4806d1a14b142
Manuel <manuel.buchauer@gmx.at>
2019-12-27 11:34:04 +0100

    analysis for code complexity

--- repository.py
+++ repository.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[41]:
+# In[2]:
 
 
 from git import Repo
@@ -10,6 +10,7 @@ import os
 import pygount
 import json
 from pathlib import Path
+import lizard
 
 class Repository:
     
@@ -21,6 +22,9 @@ class Repository:
         self.codelines = 0
         self.emptylines = 0
         self.commentlines = 0
+        
+        # average cyclomatic complexity per repository
+        self.averageCC = 0
         
         
     def download(self):
@@ -54,6 +58,42 @@ class Repository:
             
         print("Analysed Repository: ", self.repository.name)
         return
+    
+    def analyseComplexity(self):
+        ending = ''
+        
+        if self.repository.language == "Python":
+            ending = '**/*.py'
+        elif self.repository.language == "Java":
+            ending = '**/*.java'
+        else:
+            print("Error in analyse: language of repository matches neither Python nor Java")
+            return
+
+        directory_in_str = self.path+self.repository.name
+
+        pathlist = Path(directory_in_str).glob(ending)
+        
+        sum = 0
+        function_count = 0
+        
+        for path in pathlist:
+            # because path is object not string
+            path_in_str = str(path)
+            
+           
+            analysis = lizard.analyze_file(path_in_str)
+            for element in analysis.function_list:
+                sum += element.__dict__['cyclomatic_complexity']
+                function_count += 1
+            
+        
+        self.averageCC = sum/function_count
+        
+        print("Analysed Complexity of Repository: ", self.repository.name)
+        return
+        
+    
                     
     def delete(self):
         shutil.rmtree(self.path+self.repository.name)
@@ -70,6 +110,7 @@ class Repository:
                     'codelines': self.codelines,
                     'commentlines': self.commentlines,
                     'emptylines': self.emptylines,
+                    'averageCC': self.averageCC,
         }
             
         with open(self.analysisFile, 'a+') as f:
